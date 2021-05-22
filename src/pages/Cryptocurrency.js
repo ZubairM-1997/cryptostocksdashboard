81ce3c70c0cf5966557ca9e0d1f0e2dc5515712d
Zubair <zubair.maqsood6@gmail.com>
2021-05-23 00:36:43 +0100

    starting to add cryptocurrency page

--- src/pages/Cryptocurrency.js
+++ src/pages/Cryptocurrency.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function Cryptocurrency() {
-	return (
-		<div className='cryptocurrency'>
-			<h1>Cryptocurrency</h1>
-		</div>
-	)
+
+class Cryptocurrency extends Component {
+
+	componentDidMount(){
+		this.fetchData()
+
+	}
+
+	async fetchData(){
+		const url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+		const ApiKey = "976f1416-c7c4-4c71-8c2d-f5f0eb19d670"
+		const qString =  "&start=1limit=5&convert=USD"
+		try {
+			let response = await axios.get(url + qString, {
+				headers: {
+					'X-CMC_PRO_API_KEY': ApiKey
+				}
+			})
+			console.log(response)
+		} catch(err) {
+			console.log(err)
+		}
+	}
+
+	render(){
+		return (
+			<div className='cryptocurrency'>
+
+				<h1>Cryptocurrencies</h1>
+			</div>
+		)
+	}
+
 }
 
 export default Cryptocurrency
