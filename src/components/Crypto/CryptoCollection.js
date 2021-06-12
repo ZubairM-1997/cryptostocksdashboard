4153c09f828f4513c2ef9fe9f7bb2f4dce11ac4c
Zubair <zubair.maqsood6@gmail.com>
2021-06-12 20:08:37 +0100

    made crypto collection div draggable

--- src/components/Crypto/CryptoCollection.js
+++ src/components/Crypto/CryptoCollection.js
@@ -3,9 +3,53 @@ import CryptoItem from "./CryptoItem"
 import "./Crypto.css"
 
 export default class MostTraded extends Component {
+
+	constructor(props){
+		super(props)
+		this.state = {
+			diffX: 0,
+			diffY: 0,
+			dragging: false,
+			styles: {}
+		}
+
+		this._dragStart = this._dragStart.bind(this);
+		this._dragging = this._dragging.bind(this);
+		this._dragEnd = this._dragEnd.bind(this)
+	}
+
+	_dragStart(e){
+		this.setState({
+			diffX: e.screenX - e.currentTarget.getBoundingClientRect().left,
+			diffY: e.screenY - e.currentTarget.getBoundingClientRect().top,
+			dragging: true
+		})
+	}
+
+	_dragging(e){
+		if (this.state.dragging){
+			let left = e.screenX - this.state.diffX
+			let top = e.screenY - this.state.diffY
+			this.setState({
+				styles: {
+					left: left,
+					top: top
+				}
+			})
+
+		}
+
+	}
+
+	_dragEnd(){
+		this.setState({
+			dragging: false
+		})
+	}
+
 	render(){
 		return (
-			<div className="cryptoCollection">
+			<div className="cryptoCollection" style={this.state.styles} onMouseDown={this._dragStart} onMouseMove={this._dragging} onMouseUp={this._dragEnd}>
 				{
 					this.props.cryptoData.map(dataPoint => {
 						return <CryptoItem
